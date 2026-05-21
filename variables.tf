6099cd234efb69d27fb577c8fa5eb120d669e932
Brad Sickles <brad.sickles@gmail.com>
2026-05-20 20:10:05 -0400

    Renamed post_app_metadata to app_infra.

--- variables.tf
+++ variables.tf
@@ -8,7 +8,7 @@ EOF
   default = {}
 }
 
-variable "post_app_metadata" {
+variable "app_infra" {
   description = <<EOF
 Nullstone automatically injects metadata from the app module into this module through this variable.
 This variable injects specific metadata from the primary app infrastructure as a way to avoid cyclical dependencies.
@@ -89,7 +89,7 @@ locals {
   job_id       = lookup(var.app_metadata, "job_id", "")
   service_name = lookup(var.app_metadata, "service_name", "")
 
-  service_url  = lookup(var.post_app_metadata, "service_url", "")
+  service_url  = lookup(var.app_infra, "service_url", "")
 
   target_type = local.job_name != "" ? "job" : "service"
 
